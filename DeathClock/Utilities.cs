31bf5bce3946fcd968a40f79d7433c0f0e9285a0
RichTeaMan <thomasholmes@outlook.com>
2013-10-25 15:02:40 +0100

    GetPage now automatically handles wikipedia redirects.

--- DeathClock/Utilities.cs
+++ DeathClock/Utilities.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DeathClock
@@ -11,6 +12,8 @@ namespace DeathClock
     public class Utilities
     {
         static string apiUrl = "http://en.wikipedia.org/w/api.php?format=json&action=query&titles={0}&prop=revisions&rvprop=content";
+        static Regex redirectRegex = new Regex(@"(?<=#REDIRECT \[\[)[^\]]+");
+
 
         public static string GetPage(string title)
         {
@@ -18,15 +21,32 @@ namespace DeathClock
                 Directory.CreateDirectory("Cache");
 
             string cacheFile = string.Format("Cache/{0}.txt", title);
+
+            string contents;
+
             if (File.Exists(cacheFile))
-                return File.ReadAllText(cacheFile);
+            {
+                contents = File.ReadAllText(cacheFile);
+            }
+            else
+            {
+                WebClient client = new WebClient();
+                client.Headers.Add("User-Agent", "DeathClock");
+                string url = string.Format(apiUrl, title);
+                contents = client.DownloadString(url);
 
-            WebClient client = new WebClient();
-            client.Headers.Add("User-Agent", "DeathClock");
-            string url = string.Format(apiUrl, title);
-            string contents = client.DownloadString(url);
+                File.WriteAllText(cacheFile, contents);
+            }
+            var redirect = redirectRegex.Match(contents);
+            if (redirect.Success)
+            {
+                string redirectTitle = redirect.Value.Replace(' ', '_');
 
-            File.WriteAllText(cacheFile, contents);
+                // delete required to handle Windows case-insensitve file system
+                if(title.ToLowerInvariant() == redirectTitle.ToLowerInvariant())
+                    File.Delete(cacheFile);
+                contents = GetPage(redirectTitle);
+            }
             return contents;
         }
     }
